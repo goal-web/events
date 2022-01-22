54b6fc0c7ac26f05239f2b9dd8a943e83341809e
qbhy <96qbhy@gmail.com>
2022-01-22 14:06:34 +0800

    feat: 优化事件map

--- dispatcher.go
+++ dispatcher.go
@@ -3,27 +3,34 @@ package events
 import (
 	"fmt"
 	"github.com/goal-web/contracts"
+	"sync"
 )
 
 func NewDispatcher(handler contracts.ExceptionHandler) contracts.EventDispatcher {
 	return &EventDispatcher{
-		eventListenersMap: make(map[string][]contracts.EventListener, 0),
+		eventListenersMap: sync.Map{},
 		exceptionHandler:  handler,
 	}
 }
 
 type EventDispatcher struct {
-	eventListenersMap map[string][]contracts.EventListener
+	eventListenersMap sync.Map
 
 	// 依赖异常处理器
 	exceptionHandler contracts.ExceptionHandler
 }
 
-func (dispatcher EventDispatcher) Register(name string, listener contracts.EventListener) {
-	dispatcher.eventListenersMap[name] = append(dispatcher.eventListenersMap[name], listener)
+func (dispatcher *EventDispatcher) Register(name string, listener contracts.EventListener) {
+	dispatcher.eventListenersMap.Store(name, append(dispatcher.getListeners(name), listener))
+}
+func (dispatcher *EventDispatcher) getListeners(name string) []contracts.EventListener {
+	if value, exists := dispatcher.eventListenersMap.Load(name); exists {
+		return value.([]contracts.EventListener)
+	}
+	return nil
 }
 
-func (dispatcher EventDispatcher) Dispatch(event contracts.Event) {
+func (dispatcher *EventDispatcher) Dispatch(event contracts.Event) {
 	// 处理异常
 	defer func() {
 		if err := recover(); err != nil {
@@ -39,13 +46,13 @@ func (dispatcher EventDispatcher) Dispatch(event contracts.Event) {
 
 	if _, isSync := event.(contracts.SyncEvent); isSync {
 		// 同步执行事件
-		for _, listener := range dispatcher.eventListenersMap[event.Event()] {
+		for _, listener := range dispatcher.getListeners(event.Event()) {
 			listener.Handle(event)
 		}
 	} else {
 		// 协程执行
 		go func() {
-			for _, listener := range dispatcher.eventListenersMap[event.Event()] {
+			for _, listener := range dispatcher.getListeners(event.Event()) {
 				listener.Handle(event)
 			}
 		}()
--- go.mod
+++ go.mod
@@ -2,4 +2,5 @@ module github.com/goal-web/events
 
 go 1.17
 
-require github.com/goal-web/contracts v0.0.0-20220120093117-be8f72613bff // indirect
+require github.com/goal-web/contracts v0.1.7
+
